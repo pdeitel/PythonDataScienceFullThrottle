9d88bac3c3f17d22bac8902a11252e5a29d86f64
pdeitel <paul@deitel.com>
2022-08-19 09:45:27 -0400

    Update digits3d.py

--- ch14/digits3d.py
+++ ch14/digits3d.py
@@ -8,7 +8,6 @@ tsne3 = TSNE(n_components=3, random_state=11)
 reduced_data3 = tsne3.fit_transform(digits.data) 
 figure = plt.figure(figsize=(7, 5))
 axes = figure.add_subplot(projection='3d')
-axes = axes3d.Axes3D(figure)
 dots = axes.scatter(reduced_data3[:, 0], reduced_data3[:, 1], reduced_data3[:, 2], 
     c=digits.target, cmap=plt.cm.get_cmap('nipy_spectral_r', 10))
 colorbar = plt.colorbar(dots)
